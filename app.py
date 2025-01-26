65ab52f1d2358468e4f719c9268644b70778c225
rachelchi1210 <rachelchi1210@gmail.com>
2025-01-25 20:37:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if 'current_word_input' not in st.session_state:
     st.session_state['current_word_input'] = ""
 
 # Input for grid size and words
-grid_size = st.number_input("Grid Size (10-20):", min_value=6, max_value=20, value=st.session_state['current_grid_size'])
+grid_size = st.number_input("Grid Size (6-20):", min_value=6, max_value=20, value=st.session_state['current_grid_size'])
 word_input = st.text_area("Enter words (comma separated):", value=st.session_state['current_word_input'])
 
 # Buttons for generating puzzle
