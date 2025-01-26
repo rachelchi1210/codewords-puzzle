95c6bb7ed8e2e72b3480c673fd7065f318c3e3f7
rachelchi1210 <rachelchi1210@gmail.com>
2025-01-26 13:11:02 -0500

    Update codewords_puzzle_gen.py

--- codewords_puzzle_gen.py
+++ codewords_puzzle_gen.py
@@ -7,29 +7,59 @@ def generate_codewords_puzzle(word_list, grid_size):
 
     def can_place_word(word, row, col, direction):
         """Check if a word can be placed at the given row and col in the given direction."""
-        if direction == "H":
+        if direction == "H":  # Horizontal Left to Right
             if col + len(word) > grid_size:
                 return False
             for i in range(len(word)):
                 if grid[row][col + i] != '#' and grid[row][col + i] != word[i]:
                     return False
-        elif direction == "V":
+        elif direction == "HR":  # Horizontal Right to Left
+            if col - len(word) < -1:
+                return False
+            for i in range(len(word)):
+                if grid[row][col - i] != '#' and grid[row][col - i] != word[i]:
+                    return False
+        elif direction == "V":  # Vertical Top to Bottom
             if row + len(word) > grid_size:
                 return False
             for i in range(len(word)):
                 if grid[row + i][col] != '#' and grid[row + i][col] != word[i]:
                     return False
-        elif direction == "D":  # Diagonal placement
+        elif direction == "VR":  # Vertical Bottom to Top
+            if row - len(word) < -1:
+                return False
+            for i in range(len(word)):
+                if grid[row - i][col] != '#' and grid[row - i][col] != word[i]:
+                    return False
+        elif direction == "D":  # Diagonal Top-Left to Bottom-Right
             if row + len(word) > grid_size or col + len(word) > grid_size:
                 return False
             for i in range(len(word)):
                 if grid[row + i][col + i] != '#' and grid[row + i][col + i] != word[i]:
                     return False
+        elif direction == "DR":  # Diagonal Bottom-Right to Top-Left
+            if row - len(word) < -1 or col - len(word) < -1:
+                return False
+            for i in range(len(word)):
+                if grid[row - i][col - i] != '#' and grid[row - i][col - i] != word[i]:
+                    return False
+        elif direction == "RD":  # Reverse Diagonal Top-Right to Bottom-Left
+            if row + len(word) > grid_size or col - len(word) < -1:
+                return False
+            for i in range(len(word)):
+                if grid[row + i][col - i] != '#' and grid[row + i][col - i] != word[i]:
+                    return False
+        elif direction == "RDR":  # Reverse Diagonal Bottom-Left to Top-Right
+            if row - len(word) < -1 or col + len(word) > grid_size:
+                return False
+            for i in range(len(word)):
+                if grid[row - i][col + i] != '#' and grid[row - i][col + i] != word[i]:
+                    return False
         return True
 
     def place_word(word):
         """Try placing a word in random positions and directions."""
-        directions = ["H", "V", "D"]
+        directions = ["H", "HR", "V", "VR", "D", "DR", "RD", "RDR"]
         random.shuffle(directions)
         positions = [(r, c) for r in range(grid_size) for c in range(grid_size)]
         random.shuffle(positions)
@@ -40,10 +70,20 @@ def generate_codewords_puzzle(word_list, grid_size):
                     for i in range(len(word)):
                         if direction == "H":
                             grid[row][col + i] = word[i]
+                        elif direction == "HR":
+                            grid[row][col - i] = word[i]
                         elif direction == "V":
                             grid[row + i][col] = word[i]
+                        elif direction == "VR":
+                            grid[row - i][col] = word[i]
                         elif direction == "D":
                             grid[row + i][col + i] = word[i]
+                        elif direction == "DR":
+                            grid[row - i][col - i] = word[i]
+                        elif direction == "RD":
+                            grid[row + i][col - i] = word[i]
+                        elif direction == "RDR":
+                            grid[row - i][col + i] = word[i]
                     return True
         return False
 
